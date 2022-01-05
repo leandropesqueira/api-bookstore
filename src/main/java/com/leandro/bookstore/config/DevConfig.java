80182ecf93f022c9ad02f5135b7d4138532ca4de
leandropesqueira <leandropesqueira7@gmail.com>
2022-01-05 10:53:06 -0300

    Criacao do perfil de dev

---
+++ src/main/java/com/leandro/bookstore/config/DevConfig.java
@@ -0,0 +1,28 @@
+package com.leandro.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.leandro.bookstore.service.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instanciaBaseDeDados() {
+		if(strategy.equals("create")) {
+			this.dbService.instanciaBaseDeDados();
+		}
+		return false;		
+	}
+}
