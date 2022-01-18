1ee06b2f3c709ae0a9fe8425c036933985060cd4
leandropesqueira <leandropesqueira7@gmail.com>
2022-01-18 08:57:10 -0300

    findAll para Categoria utilizando padrao DTO

--- src/main/java/com/leandro/bookstore/resource/CategoriaResource.java
+++ src/main/java/com/leandro/bookstore/resource/CategoriaResource.java
@@ -1,5 +1,8 @@
 package com.leandro.bookstore.resource;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leandro.bookstore.domain.Categoria;
+import com.leandro.bookstore.dtos.CategoriaDTO;
 import com.leandro.bookstore.service.CategoriaService;
 
 @RestController
@@ -21,6 +25,14 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> findById(@PathVariable Long id){
 		Categoria obj = categoriaService.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll(){
+		List<Categoria> list = categoriaService.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
--- src/main/java/com/leandro/bookstore/service/CategoriaService.java
+++ src/main/java/com/leandro/bookstore/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.leandro.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,10 @@ public class CategoriaService {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public List<Categoria> findAll(){
+		return categoriaRepository.findAll();
 	}
 
 }
