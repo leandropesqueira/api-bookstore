a38f0b7c9b1ffcdd6c61d2f7e0707bdde2cb778f
leandropesqueira <leandropesqueira7@gmail.com>
2022-01-24 10:55:39 -0300

    Deploy no Heroku

--- src/main/java/com/leandro/bookstore/service/DBService.java
+++ src/main/java/com/leandro/bookstore/service/DBService.java
@@ -22,7 +22,8 @@ public class DBService {
 	public void instanciaBaseDeDados() {
 		Categoria cat1 = new Categoria(null, "Informática", "Livro de TI");
 		Categoria cat2 = new Categoria(null, "Ficção Científica", "Ficção Científica");
-		Categoria cat3 = new Categoria(null, "Biografias", "Livro de Biografias");		
+		Categoria cat3 = new Categoria(null, "Biografias", "Livro de Biografias");
+		Categoria cat4 = new Categoria(null, "Banco de Dados", "Livro de DB");
 		
 		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
 		Livro l2 = new Livro(null, "Engenharia de Software", "Louis V. Gerstner", "Lorem ipsum", cat1);
@@ -30,13 +31,15 @@ public class DBService {
 		Livro l4 = new Livro(null, "The War of the Worlds", "H.G. Wells", "Lorem ipsum", cat2);
 		Livro l5 = new Livro(null, "I, Robot", "Isaac Asimov", "Lorem ipsum", cat2);
 		Livro l6 = new Livro(null, "Life of Author", "H.G. Wells", "Lorem ipsum", cat3);
+		Livro l7 = new Livro(null, "PostgreSQL Guia Fundamental", "Carlos Pampulim Caldeira", "Lorem ipsum", cat4);
 		
 		cat1.getLivros().addAll(Arrays.asList(l1,l2));
 		cat2.getLivros().addAll(Arrays.asList(l3,l4,l5));
 		cat3.getLivros().addAll(Arrays.asList(l6));
+		cat4.getLivros().addAll(Arrays.asList(l7));
 		
-		this.categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
-		this.livroRepository.saveAll(Arrays.asList(l1,l2,l3,l4,l5,l6));
+		this.categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3,cat4));
+		this.livroRepository.saveAll(Arrays.asList(l1,l2,l3,l4,l5,l6,l7));
 	}
 	
 }
